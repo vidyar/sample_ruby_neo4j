2516fd87d1973add544b3de4cc7a3314e820c9b9
ric03uecS <devashish+shippable@shippable.com>
2014-10-01 11:44:15 +0530

    Update sample.rb

--- sample.rb
+++ sample.rb
@@ -3,19 +3,24 @@ require 'neo4j'
 puts "starting neo4j buildsample written using ruby.."
 
 puts "Creating sample nodes with properties name and age.."
+
+session = Neo4j::Session.open(:server_db, "http://localhost:7474")
 Neo4j::Transaction.run do
-  node1 = Neo4j::Node.new(:name => 'uone', :age => 10)
-  node2 = Neo4j::Node.new(:name => 'utwo', :age => 20)
-  node3 = Neo4j::Node.new(:name => 'uthree', :age => 30)
-  node4 = Neo4j::Node.new(:name => 'ufour', :age => 40)
+  node1 = Neo4j::Node.create({name: 'uone', age: 10})
+  node2 = Neo4j::Node.create({name: 'utwo', age: 15})
+  node3 = Neo4j::Node.create({name: 'uthree', age: 30})
+  node4 = Neo4j::Node.create({name: 'ufour', age: 40})
 
   puts "Assigning relationship between nodes.."
 
-  node1.both(:friends) << node2
-  node2.both(:friends) << node3
-  node3.both(:friends) << node4
+
+  Neo4j::Relationship.create(:friends, node1, node2)
+  Neo4j::Relationship.create(:friends, node2, node3)
+  Neo4j::Relationship.create(:friends, node3, node4)
+  Neo4j::Relationship.create(:friends, node1, node4)
 
   puts "Output from the sample program: "
-
-  puts node1.outgoing(:friends).depth(3).map{|node| node[:name]}.join(" => ")
+  
+  rels = node1.rels(dir: :outgoing, type: :friends)
+  puts rels
 end
